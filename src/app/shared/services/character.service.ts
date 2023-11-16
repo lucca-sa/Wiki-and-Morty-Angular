1f14ccb2c1ddf99ba9d0ecf1a86d2eb7feaf5ea0
lucca-sa <scherer.lucca@gmail.com>
2023-11-16 17:18:02 -0300

    Code Documentation

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -11,11 +11,14 @@ export class CharacterService {
   constructor(private http: HttpClient) {}
 
   searchCharacters(query = '', page = 1) {
+    // Constrói a URL da API com o termo de pesquisa e número da página
     const filter = `${environment.baseUrlAPI}character/?name=${query}&page=${page}`;
+    // Realiza a chamada GET à API e retorna a resposta como um array de personagens
     return this.http.get<Character[]>(filter);
   }
 
   getDetails(id: number) {
+    // Constrói a URL da API para obter detalhes de um personagem com base no ID
     return this.http.get<Character>(`${environment.baseUrlAPI}character/${id}`);
   }
 }
--- src/app/shared/services/location.service.ts
+++ src/app/shared/services/location.service.ts
@@ -11,11 +11,14 @@ export class LocationService {
   constructor(private http: HttpClient) {}
 
   searchLocations(query = '', page = 1) {
+    // Constrói a URL da API com o termo de pesquisa e número da página
     const filter = `${environment.baseUrlAPI}location/?name=${query}&page=${page}`;
+    // Realiza a chamada GET à API e retorna a resposta como um array de locações
     return this.http.get<Location[]>(filter);
   }
 
   getDetails(id: number) {
+    // Constrói a URL da API para obter detalhes de uma localização com base no ID
     return this.http.get<Location>(`${environment.baseUrlAPI}location/${id}`);
   }
 }
